6ea10c83fe0988de9c34ef3874cb0bc1f73576c7
schourode <jsr@malamute.dk>
2011-07-01 22:58:07 +0200

    Simple, yet effective approach to blocking (an area of) the screen.

---
+++ jquery.shield.js
@@ -0,0 +1,21 @@
+﻿/*!
+* jQuery.shield() plugin
+* Copyright 2011, jsr@malamute.dk
+* Dual licensed under the MIT or GPL Version 2 licenses.
+*/
+
+/*global jQuery*/
+
+(function ($) {
+    'use strict';
+
+    $.fn.shield = function (className) {
+        return $('<div>').addClass(className || 'shield').css({
+            'position': 'absolute',
+            'width': this.outerWidth(),
+            'height': this.outerHeight(),
+            'left': this.position().left,
+            'top': this.position().top
+        }).insertAfter(this);
+    };
+})(jQuery);
